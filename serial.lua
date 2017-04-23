fa89684569aa4234b486f3406e2340f204e1ca0b
Anthony <0xsoto@gmail.com>
2017-04-22 20:39:02 -0400

    Added: basic user input check TODO for it's current limitation

--- serial.lua
+++ serial.lua
@@ -15,7 +15,6 @@ else print(string.format("Connection to %s established",port_name))
    
 end
 
-
 assert(p:set_baud_rate(rs232.RS232_BAUD_9600) == rs232.RS232_ERR_NOERROR)
 assert(p:set_data_bits(rs232.RS232_DATA_8) == rs232.RS232_ERR_NOERROR)
 assert(p:set_parity(rs232.RS232_PARITY_NONE) == rs232.RS232_ERR_NOERROR)
@@ -24,21 +23,37 @@ assert(p:set_flow_control(rs232.RS232_FLOW_OFF) == rs232.RS232_ERR_NOERROR)
 
 
 
+io.write("Please Enter the key you would like to use. \n In the form of A for the letter A. \n If you would like to use a special character please spell out the character .e.g GRAVE for '`' \n :")
+
+_input = io.read()
+
+function keyNum()
+
+   for key , value in pairs(require("uinput")) do
+      if key:match("_".._input .."$") then  
+	 local num = value
+	 return num
+      end
+
+   end
+
+end
+
+
 while true do
 
    if p:read(1) == 0 then 
 
-      keyboard:press(uinput.KEY_A)
-
-
-
+      keyboard:press(keyNum())
    
    else
       
-      keyboard:release(uinput.KEY_A)
+      keyboard:release(keyNum())
+   end
 
-      end
 end
+
+
 
 
 
